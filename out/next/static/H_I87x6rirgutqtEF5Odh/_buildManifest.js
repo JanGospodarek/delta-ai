00569d6c0cf7db1b5ba9eb2a0a59e5aee370c4ea
JanGospodarek <jan.gospo@onet.pl>
2025-01-11 00:31:16 +0100

    feat(redux): add redux state and save api key

---
+++ out/next/static/H_I87x6rirgutqtEF5Odh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/823-4a13cdc3bbed8d2b.js","static/chunks/pages/index-5879684f21c3e35c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
-
+import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
+import StoreProvider from '../store/StoreProvider';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: any) {
   return (
     <NextUIProvider>
-      <div className="w-[300px] h-[400px] bg-white border-2 border-amber-500">
-        <Component {...pageProps} />
-      </div>
+      <StoreProvider>
+        <div className="w-[300px] h-[400px] bg-white border-2 border-amber-500">
+          <Component {...pageProps} />
+        </div>
+      </StoreProvider>
     </NextUIProvider>
   );
 }
